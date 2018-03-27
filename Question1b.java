cf2118687d8eb0a52704525ebfd102ac933e75ba
staszqa <37718487+staszqa@users.noreply.github.com>
2018-03-26 23:03:22 -0400

    Add files via upload

---
+++ Question1b.java
@@ -0,0 +1,27 @@
+package Exemple.WeSrudy;
+import java .io.*;
+import java .util.*;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Question1b {
+
+	private Scanner x ;
+	
+	public void readFile() {
+		
+		
+		x = new ((new file("words.txt"));
+		
+	}
+	
+	
+	
+
+	
+	
+
+}
---
+++ Question3.java
@@ -0,0 +1,28 @@
+package Exemple.WeSrudy;
+
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class Question3 {
+	
+	
+	public static void main(String[] args) {
+		
+		Random r = new Random();
+		for (int i = 0; i< 500 ; i++){
+			
+			System.out.println(r.nextInt(10));
+		
+		
+	}
+	
+	}
+	
+	
+}
+	
+	
+	
+	
